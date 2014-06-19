b2d61de1cb7a12f787a96842666b29e80ac4e221
hagateby <hagateby@gmail.com>
2014-06-20 00:05:12 +0800

    Initial Commit

---
+++ EFlute/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EFlute
+//
+//  Created by liuwh on 14-6-20.
+//  Copyright (c) 2014年 liuwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EFlute/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EFlute
+//
+//  Created by liuwh on 14-6-20.
+//  Copyright (c) 2014年 liuwh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
